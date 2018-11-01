30cc9d7e9d478e7e248d85fddf148b5758bd9958
家凡 王 <1105105303@office365.kuas.edu.tw>
2018-11-01 20:55:53 +0800

    add 1005's hw

---
+++ 1005/1005/Models/OpenData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace _1005.Models
+{
+    public class OpenData
+    {
+        /*private int _id;
+        public int Id {
+            get {
+                return _id;
+            }
+            set {
+                this._id = value;
+            }
+        }*/
+        public int id { get; set; }
+        public string 資料集名稱 { get; set; }
+        public string 主要欄位說明 { get; set; }
+        public string 服務分類 { get; set; }
+
+
+        /* public string Get資料集名稱() {
+             return "";
+         }*/
+    }
+}
